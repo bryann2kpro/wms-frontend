433d8edf05f8cbf8f13658297e20d7945080f338
arispelgremory <hiimyino0503@gmail.com>
2026-01-21 08:47:44 +0800

    refactor(routes): simplify login redirection by using beforeLoad in route definition

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,7 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
-import { useEffect } from "react";
 
-export const Route = createFileRoute("/")({ component: App });
-
-function App() {
-	useEffect(() => {
-		console.log("redirecting to /login");
+export const Route = createFileRoute("/")({ 
+	beforeLoad: async () => {
 		throw redirect({ to: "/login" });
-	}, []);
-
-	return null;
-}
+	},
+ });
