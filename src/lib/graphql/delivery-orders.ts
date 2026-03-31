4ce3ee5c566b99246c1bc7f8b69287d6b81119f9
arispelgremory <hiimyino0503@gmail.com>
2026-03-31 14:50:34 +0800

    feat(picking-list): add mutation and UI for generating delivery order picking list

--- src/lib/graphql/delivery-orders.ts
+++ src/lib/graphql/delivery-orders.ts
@@ -201,6 +201,22 @@ export const ALLOCATE_PICK_LIST_MUTATION = gql`
 	${DELIVERY_ORDER_ITEM_WITH_DETAILS_FRAGMENT}
 `;
 
+export const GENERATE_DO_PICKING_LIST_MUTATION = gql`
+	mutation GenerateDoPickingList {
+		generateDoPickingList {
+			pdfBase64
+			filename
+		}
+	}
+`;
+
+export type GenerateDoPickingListMutationData = {
+	generateDoPickingList: {
+		pdfBase64: string;
+		filename: string;
+	};
+};
+
 // ---------------------------------------------------------------------------
 // Types
 // ---------------------------------------------------------------------------
